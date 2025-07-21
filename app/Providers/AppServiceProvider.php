7e6e825d45272b9826d2bc4808dc1bb8e56de09f
MochSiddiq7 <139839544+MochSiddiq7@users.noreply.github.com>
2025-07-22 03:10:44 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Events\ConnectionEstablished;
-
-
-
+use Illuminate\Support\ServiceProvider; // ✅ Tambahkan ini!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,20 +21,18 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
- 
-
-     public function boot(): void
-{
-    if ($this->app->environment('production')) {
-        URL::forceScheme('https');
-    }
-
-    // Jalankan hanya saat koneksi database berhasil
-    Event::listen(ConnectionEstablished::class, function (ConnectionEstablished $event) {
-        if ($event->connection->getDriverName() === 'pgsql') {
-            $schema = env('DB_SCHEMA', 'public');
-            $event->connection->statement("SET search_path TO {$schema}");
+    public function boot(): void
+    {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
-    });
-}
+
+        // Jalankan hanya saat koneksi database berhasil
+        Event::listen(ConnectionEstablished::class, function (ConnectionEstablished $event) {
+            if ($event->connection->getDriverName() === 'pgsql') {
+                $schema = env('DB_SCHEMA', 'public');
+                $event->connection->statement("SET search_path TO {$schema}");
+            }
+        });
+    }
 }
