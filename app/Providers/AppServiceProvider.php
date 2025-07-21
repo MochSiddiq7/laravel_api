dd82f81ff14c6c5512445e800f62e31a5a43719f
mochsiddiq7 <siddiqhamid77@gmail.com>
2025-07-22 02:13:50 +0700

    Initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\DB; // <- letakkan di sini, DI LUAR class
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika bukan local
+        if (env('APP_ENV') !== 'local' && env('APP_URL') !== 'http://127.0.0.1:8000') {
+            URL::forceScheme('https');
+        }
+
+        // Ganti schema ke "reservasi"
+        DB::statement('SET search_path TO reservasi');
     }
 }
