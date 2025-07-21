780dba8e77a7296fa5baf177064fa5aabbc8d92e
MochSiddiq7 <139839544+MochSiddiq7@users.noreply.github.com>
2025-07-22 02:31:06 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,14 +22,21 @@ class AppServiceProvider extends ServiceProvider
  
 
     public function boot(): void
-    {
-        // Paksa HTTPS jika bukan local
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
-    
-        // SET schema PostgreSQL ke 'reservasi'
-        DB::statement('SET search_path TO reservasi');
+{
+    // Paksa HTTPS jika di production
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
+
+    // Jalankan SET search_path hanya jika pakai PostgreSQL dan database terkoneksi
+    try {
+        if (DB::connection()->getDriverName() === 'pgsql') {
+            DB::statement('SET search_path TO reservasi');
+        }
+    } catch (\Exception $e) {
+        // Biarkan kosong, artinya jika belum konek DB (saat package:discover), jangan error
+    }
+}
+
     
 }
